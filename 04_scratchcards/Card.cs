2ed1a64e24a0385c722c0749f9791aacfefe0730
Tobias Meier <tmeier@techgroup.ch>
2023-12-04 17:23:33 +0100

    day4

---
+++ 04_scratchcards/Card.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_scratchcards;
+
+internal class Card
+{
+    internal int Id { get; }
+    internal IEnumerable<int> WinNumbers { get; }
+    internal IEnumerable<int> ScratchNumbers { get; }
+
+    internal int Copies { get; private set; }
+
+    internal void AddCopies(int i)
+    {
+        Copies +=i;
+    }
+
+    internal Card(int id, IEnumerable<int> winNumbers, IList<int> scratchNumbers)
+    {
+        Id = id;
+        WinNumbers = winNumbers.Distinct();
+        ScratchNumbers = scratchNumbers.Distinct();
+        Copies = 1;
+    }
+
+    internal int Matches()
+    {
+       return WinNumbers.Intersect(ScratchNumbers).AsEnumerable().Count();
+    }
+
+    internal int Points()
+    {
+        var commonNumbers = WinNumbers.Intersect(ScratchNumbers).AsEnumerable();
+        int count = commonNumbers.Count();
+
+        if (count == 0)
+        {
+            return 0;
+        }
+
+        var points = 1 * ((int)Math.Pow(2, count - 1));
+
+        return points;
+    }
+}
+
---
+++ 04_scratchcards/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices.JavaScript;
+using _04_scratchcards;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("AoC 2023 4.12.2023");
+
+// Read a text file line by line.
+var path = Path.Combine(Directory.GetCurrentDirectory(), "../../../input.txt");
+var lines = File.ReadAllLines(path);
+
+var cards = new List<Card>();
+
+foreach (var line in lines)
+{
+    //Console.WriteLine(line);
+    var cardArgs = line.Split(":");
+    var cardId = int.Parse(cardArgs[0].Trim("Card ".ToCharArray()));
+
+    var cardNumbers = cardArgs[1].Split("|");
+
+    var winNumbers = cardNumbers[0].Trim().Split(" ")
+        .Where(s=> !string.IsNullOrEmpty(s))
+        .Select(n=>
+            int.Parse(n.Trim()))
+        .ToList();
+    var scratchedNumbers = cardNumbers[1].Trim().Split(" ")
+        .Where(s => !string.IsNullOrEmpty(s))
+        .Select(n =>
+            int.Parse(n.Trim()))
+        .ToList();
+
+    cards.Add(new Card(cardId, winNumbers, scratchedNumbers));
+}
+
+var totalPoints =0;
+
+foreach (var card in cards)
+{
+    totalPoints += card.Points();
+}
+
+Console.WriteLine($"Total Points: {totalPoints}");
+
+// Pt2
+for (int i = 0; i < cards.Count; i++)
+{
+    var matches = cards[i].Matches();
+    var copies = cards[i].Copies; // The initial 
+    for (int j = 1; j <= matches; j++)
+    {
+        if (j + i >= cards.Count)
+            break;
+
+        cards[i+j].AddCopies(copies);
+    }
+
+}
+
+var totalCards = 0;
+
+foreach (var card in cards)
+{
+    totalCards += card.Copies;
+}
+
+Console.WriteLine($"Total cards: {totalCards}");
