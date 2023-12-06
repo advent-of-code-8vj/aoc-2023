940863bda49919ca5ba67b56c264911bfe95266f
Tobias Meier <tmeier@techgroup.ch>
2023-12-06 17:53:13 +0100

    day6

---
+++ 06_boat_race/Program.cs
@@ -0,0 +1,51 @@
+﻿using _06_boat_race;
+
+Console.WriteLine("AoC 2023 6.12.2023");
+
+// Read a text file line by line.
+var path = Path.Combine(Directory.GetCurrentDirectory(), "../../../input2.txt");
+var lines = File.ReadAllLines(path);
+
+var times = lines[0].Split(" ").Where(s=> !string.IsNullOrEmpty(s)).Skip(1).Select(s=> long.Parse(s)).ToList();
+var distances = lines[1].Split(" ").Where(s => !string.IsNullOrEmpty(s)).Skip(1).Select(s => long.Parse(s)).ToList();
+
+var races = new List<Tuple<long, long, int>>();
+for(int i = 0; i < times.Count; ++i)
+{
+    races.Add(new(times[i], distances[i], i));
+}
+
+
+object sync = new object();
+
+long strategyProduct = 1;
+
+Parallel.ForEach(races, race =>
+{
+    Console.WriteLine($"Calculating Race {race.Item3}");
+
+    int numberOfStrategys = 0;
+
+    for (int i = 1; i < race.Item1 - 1; ++i)
+    {
+        // velocity = accelerationTime
+        var accelerationTime = i;
+        var glidingTime = race.Item1 - i;
+        var distance = race.Item2;
+
+        if (RaceStrategy.IsSuccessful(accelerationTime, glidingTime, distance))
+        {
+            numberOfStrategys++;
+        }
+    }
+
+    lock (sync)
+    {
+        if (numberOfStrategys > 0)
+        {
+            strategyProduct *= numberOfStrategys;
+        }
+    }
+});
+
+Console.WriteLine($"Product: {strategyProduct}");
---
+++ 06_boat_race/RaceStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_boat_race;
+
+internal static class RaceStrategy
+{
+    internal static bool IsSuccessful(long velocity, long time, long requiredDistance)
+    {
+        return velocity * time > requiredDistance;
+    }
+}
+
