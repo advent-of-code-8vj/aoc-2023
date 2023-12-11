9ef38b2dcfe43cdcc0299a658cef9cd159ba9d4c
Tobias Meier <tmeier@techgroup.ch>
2023-12-11 18:07:32 +0100

    day 09

---
+++ 09_mirage_maintenance/Helper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_mirage_maintenance;
+
+internal static class Helper
+{
+    internal static int Extrapolation(int[] values)
+    {
+        if (Array.TrueForAll( values, v=> v == 0))
+        {
+            return 0;
+        }
+
+        var diffs = new int[values.Length - 1];
+        for (var i = 0; i < values.Length - 1; ++i)
+        {
+            diffs[i] = values[i + 1] - values[i];
+        }
+
+        // Last element + extrapolation
+        return diffs[^1] + Extrapolation(diffs);
+    }
+
+    internal static int ReverseExtrapolation(int[] values)
+    {
+        if (Array.TrueForAll(values, v => v == 0))
+        {
+            return 0;
+        }
+
+        var diffs = new int[values.Length - 1];
+        for (var i = 0; i < values.Length - 1; ++i)
+        {
+            diffs[i] = values[i + 1] - values[i];
+        }
+
+        // first element  - extrapolation
+        return diffs[0] - ReverseExtrapolation(diffs);
+    }
+
+}
+
---
+++ 09_mirage_maintenance/Program.cs
@@ -0,0 +1,20 @@
+﻿using _09_mirage_maintenance;
+
+Console.WriteLine("AoC 2023 9.12.2023");
+
+// Read a text file line by line.
+var path = Path.Combine(Directory.GetCurrentDirectory(), "../../../input.txt");
+var lines = File.ReadAllLines(path);
+
+var histories = lines
+    .Select(line => line.Trim().Split(" ").Select(s => int.Parse(s.Trim())).ToArray()).ToList();
+
+
+
+var sumOfPredictions = histories.Select(s => s[^1] + Helper.Extrapolation(s)).Sum();
+
+var sumOfReversePredictions = histories.Select(s => s[0] - Helper.ReverseExtrapolation(s)).Sum();
+
+Console.WriteLine($"Sum of ExtrapolatedValues: {sumOfPredictions}");
+
+Console.WriteLine($"Sum of Reverse Extrapolated Values: {sumOfReversePredictions}");
